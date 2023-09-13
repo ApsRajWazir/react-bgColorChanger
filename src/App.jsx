e9f0c889c461de39f6c8e2d9047e0cc1b1530d25
aps <amarpreet888singh@gmail.com>
2023-09-13 08:12:33 +0530

    Initial Commit - Created a react app via Vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='main'>
+      <center><h1>Hello 30 Days Of React</h1></center>
+    </div>
+  )
+}
+
+export default App
